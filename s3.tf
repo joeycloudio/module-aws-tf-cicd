47cbf72b9896db888e41c8b2a81557fa2e88f5e4
joeycloudio <joeyacostax@gmail.com>
2025-04-11 03:20:24 -0700

    updated s3.tf

--- s3.tf
+++ s3.tf
@@ -11,20 +11,20 @@ resource "aws_s3_bucket" "logging_bucket" {
 }
 
 resource "random_string" "codepipeline_artifacts_s3_buckets" {
-  for_each = var.codepipeline_pipelines == null ? {} : var.codepipeline_pipelines
+  for_each = local.safe_codepipeline_pipelines
   length   = 4
   special  = false
   upper    = false
 }
 
 resource "aws_s3_bucket" "codepipeline_artifacts_buckets" {
-  for_each = var.codepipeline_pipelines == null ? {} : var.codepipeline_pipelines
+  for_each = local.safe_codepipeline_pipelines
   bucket   = "pipeline-artifacts-${each.value.name}-${random_string.codepipeline_artifacts_s3_buckets[each.key].result}"
   force_destroy = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "codepipeline_encryption" {
-  for_each = var.codepipeline_pipelines == null ? {} : var.codepipeline_pipelines
+  for_each = local.safe_codepipeline_pipelines
 
   bucket = aws_s3_bucket.codepipeline_artifacts_buckets[each.key].id
 
@@ -42,7 +42,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "codepipeline_encr
   #checkov:skip=CKV_AWS_144: "Ensure that S3 bucket has cross-region replication enabled"
 
 resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_pabs" {
-  for_each = var.codepipeline_pipelines == null ? {} : var.codepipeline_pipelines
+  for_each = local.safe_codepipeline_pipelines
   bucket   = aws_s3_bucket.codepipeline_artifacts_buckets[each.key].id
 
   block_public_acls       = var.s3_public_access_block
@@ -52,7 +52,7 @@ resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_pabs" {
 }
 
 resource "aws_s3_bucket_versioning" "codepipeline_versioning" {
-  for_each = var.codepipeline_pipelines == null ? {} : var.codepipeline_pipelines
+  for_each = local.safe_codepipeline_pipelines
   bucket   = aws_s3_bucket.codepipeline_artifacts_buckets[each.key].id
 
   versioning_configuration {
@@ -61,7 +61,7 @@ resource "aws_s3_bucket_versioning" "codepipeline_versioning" {
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "codepipeline_lifecycle" {
-  for_each = var.codepipeline_pipelines == null ? {} : var.codepipeline_pipelines
+  for_each = local.safe_codepipeline_pipelines
   bucket   = aws_s3_bucket.codepipeline_artifacts_buckets[each.key].id
 
   rule {
@@ -83,7 +83,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "codepipeline_lifecycle" {
 }
 
 resource "aws_s3_bucket_logging" "codepipeline_logs" {
-  for_each = var.codepipeline_pipelines == null ? {} : var.codepipeline_pipelines
+  for_each = local.safe_codepipeline_pipelines
 
   bucket        = aws_s3_bucket.codepipeline_artifacts_buckets[each.key].id
   target_bucket = aws_s3_bucket.logging_bucket.id
