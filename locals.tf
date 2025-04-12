996177d41edd0b08de81c6026b7685e71641b210
joeycloudio <joeyacostax@gmail.com>
2025-04-12 01:28:55 -0700

    updated locals file

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  safe_codepipeline_pipelines = var.codepipeline_pipelines == null ? {} : nonsensitive(var.codepipeline_pipelines)
+  safe_codepipeline_pipelines = var.codepipeline_pipelines == null ? {} : var.codepipeline_pipelines
 
   tf_test_path_to_buildspec  = "${path.module}/buildspec/tf-test-buildspec.yml"
   checkov_path_to_buildspec  = "${path.module}/buildspec/checkov-buildspec.yml"
